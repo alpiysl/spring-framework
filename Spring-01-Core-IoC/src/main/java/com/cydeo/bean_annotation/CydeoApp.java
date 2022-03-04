453cc401478c57f405294f7c2015d9235fbbeb20
alpiysl <alpiysll@gmail.com>
2022-03-04 22:04:22 +0300

    multiple beans same type

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -7,14 +7,20 @@ public class CydeoApp {
     public static void main(String[] args) {
 
         //create spring container
-        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
 
         FullTimeMentor ft = container.getBean(FullTimeMentor.class);
 
         String str = container.getBean(String.class);
 //        String asd = container.getBean(String.class);
 //        System.out.println("asd = " + asd);
-        int  a =container.getBean(Integer.class);
+        int a = container.getBean(Integer.class);
+
+//        PartTimeMentor pt = container.getBean("p1",PartTimeMentor.class);
+        PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+
+        pt.createAccount();
+
         System.out.println(str);
         ft.createAccount();
         System.out.println(a);
